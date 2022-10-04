2f6212499d9100679823c0fe82b59e5264d8d208
Szymon Studniarz <szymek1019@gmail.com>
2022-10-04 10:07:31 +0200

    configure scss styling

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,23 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/preset-scss",
+    {
+      name: "storybook-addon-sass-postcss",
+      options: {
+        sassLoaderOptions: {
+          implementation: require("sass"),
+        },
+        rule: {
+          test: /\.(scss|sass)$/i,
+          use: ["style-loader", "css-loader", "sass-loader"],
+          include: path.resolve(__dirname, "../"),
+        },
+      },
+    },
   ],
   framework: "@storybook/react",
   core: {
