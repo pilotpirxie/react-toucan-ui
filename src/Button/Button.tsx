2419848d5a1d127b3751be25dc3c86a8152eed66
Szymon Studniarz <szymek1019@gmail.com>
2022-10-21 11:46:23 +0200

    lint code accordingly to eslint rules

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,25 +1,25 @@
-import cx from "classnames";
-import React from "react";
-import styles from "./button.module.scss";
+import cx from 'classnames';
+import React from 'react';
+import styles from './button.module.scss';
 
 export type ButtonProps = {
   /**
    * The color of the button.
    */
   color?:
-    | "primary"
-    | "secondary"
-    | "tertiary"
-    | "danger"
-    | "warning"
-    | "success"
-    | "info"
-    | "light"
-    | "dark";
+    | 'primary'
+    | 'secondary'
+    | 'tertiary'
+    | 'danger'
+    | 'warning'
+    | 'success'
+    | 'info'
+    | 'light'
+    | 'dark';
   /**
    * How large should the button be?
    */
-  size?: "extraSmall" | "small" | "medium" | "large" | "extraLarge";
+  size?: 'extraSmall' | 'small' | 'medium' | 'large' | 'extraLarge';
   /**
    * Should button be outlined or filled?
    */
@@ -38,8 +38,8 @@ export type ButtonProps = {
  * Primary UI component for user interaction
  */
 export function Button({
-  size = "medium",
-  color = "primary",
+  size = 'medium',
+  color = 'primary',
   outlined = false,
   children,
   ...props
@@ -48,21 +48,21 @@ export function Button({
     <button
       type="button"
       className={cx({
-        [styles.backgroundPrimary]: color === "primary",
-        [styles.backgroundSecondary]: color === "secondary",
-        [styles.backgroundTertiary]: color === "tertiary",
-        [styles.backgroundSuccess]: color === "success",
-        [styles.backgroundInfo]: color === "info",
-        [styles.backgroundWarning]: color === "warning",
-        [styles.backgroundDanger]: color === "danger",
-        [styles.backgroundLight]: color === "light",
-        [styles.backgroundDark]: color === "dark",
+        [styles.backgroundPrimary]: color === 'primary',
+        [styles.backgroundSecondary]: color === 'secondary',
+        [styles.backgroundTertiary]: color === 'tertiary',
+        [styles.backgroundSuccess]: color === 'success',
+        [styles.backgroundInfo]: color === 'info',
+        [styles.backgroundWarning]: color === 'warning',
+        [styles.backgroundDanger]: color === 'danger',
+        [styles.backgroundLight]: color === 'light',
+        [styles.backgroundDark]: color === 'dark',
 
-        [styles.sizeExtraSmall]: size === "extraSmall",
-        [styles.sizeSmall]: size === "small",
-        [styles.sizeMedium]: size === "medium",
-        [styles.sizeLarge]: size === "large",
-        [styles.sizeExtraLarge]: size === "extraLarge",
+        [styles.sizeExtraSmall]: size === 'extraSmall',
+        [styles.sizeSmall]: size === 'small',
+        [styles.sizeMedium]: size === 'medium',
+        [styles.sizeLarge]: size === 'large',
+        [styles.sizeExtraLarge]: size === 'extraLarge',
 
         [styles.outlined]: outlined === true,
       })}
