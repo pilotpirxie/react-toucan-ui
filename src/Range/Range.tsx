ae40b987fdc45ac98fff5e536a13de11fc926f2c
Szymon Studniarz <szymek1019@gmail.com>
2022-11-06 12:22:04 +0100

    remove state from component, use pure CSS to style range slider

--- src/Range/Range.tsx
+++ src/Range/Range.tsx
@@ -1,5 +1,4 @@
-import cx from 'classnames';
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './range.module.scss';
 
 export type RangeProps = {
@@ -20,32 +19,12 @@ export function Range({
   id,
   ...props
 }: RangeProps) {
-  const handleChange = (e:any) => {
-    const slider = {
-      min: 0,
-      max: 100,
-      progress: 50,
-    };
-    if (parseInt(e.target.min, 10)) {
-      slider.min = parseInt(e.target.min, 10);
-    }
-    if (parseInt(e.target.max, 10)) {
-      slider.max = parseInt(e.target.max, 10);
-    }
-    slider.progress = (((e.target.value - slider.min) / (slider.max - slider.min)) * 100);
-    document.documentElement.style.setProperty('--progress', `${slider.progress}%`);
-  };
   return (
-    <div className={styles.slider}>
-      <div className={styles.progress}>
-        <input
-          type="range"
-          id={id}
-          className={styles.range}
-          onChange={(e) => handleChange(e)}
-          {...props}
-        />
-      </div>
-    </div>
+    <input
+      type="range"
+      id={id}
+      className={styles.range}
+      {...props}
+    />
   );
 }
